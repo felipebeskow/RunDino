cea6ef4bef318009d3ead6da5a4b18d111a33a20
Felipe Beskow <felipebeskow@outlook.com>
2021-04-10 17:20:34 -0300

    Musica e nova scene

---
+++ Assets/SceneManagerMeteoroVindoScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerMeteoroVindoScript : MonoBehaviour {
+    
+    public Transform meteoro;
+
+    void Start(){
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update() {
+        if (meteoro.position.z >= 5000) {
+            SceneManager.LoadScene("RunDino");
+        }
+        
+        /*
+        if(SceneManager.GetActiveScene().name == "NomeDaCenaDesejada") {
+            source.Stop();
+        }
+        */
+    }
+}
--- Assets/Scripts/SceneManagerMenuInicialScript.cs
+++ Assets/Scripts/SceneManagerMenuInicialScript.cs
@@ -1,16 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneManagerMenuInicialScript : MonoBehaviour {
     
-    public GameObject meteoro;
-
-    private void Start() {
-        meteoro.GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * 0.5f;
-    }
     public void iniciar() {
         SceneManager.LoadScene("MeteoroVindo");
     }
